c57fa96df32d76dfca8912a902dfc4c2b9992f50
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-08-08 10:55:13 +0530

    Revert to simplest auth configuration

--- convex/auth.ts
+++ convex/auth.ts
@@ -3,9 +3,4 @@ import { convexAuth } from "@convex-dev/auth/server";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [Password],
-  callbacks: {
-    async createOrUpdateUser(ctx, args) {
-      return args.existingUserId ?? (await ctx.db.insert("users", args.profile));
-    },
-  },
 });
